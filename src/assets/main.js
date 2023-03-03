dbffca4dc2c6df8d0848bde65c15134c539d8a36
Galenso <galensoserrano1@gmail.com>
2023-03-03 12:34:42 -0300

    Avergaston

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,13 @@
-const API = "https://youtube-v31.p.rapidapi.com/search?channelId=UCCrOvNWg_26y7LjNm34HmeA&part=snippet%2Cid&order=date&maxResults=9";
+const API = "https://youtube-v31.p.rapidapi.com/search?channelId=UCZ1XcnNHSBsu7DVndIyAryA&part=snippet%2Cid&order=date&maxResults=9";
 const content = null || document.getElementById("content");
 const options = {
 	method: 'GET',
 	headers: {
+        
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+	
 		'X-RapidAPI-Key': '8d28a1d9b1msh89131d0e11da97cp15a825jsn581c375a0ef5',
-		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-	}
+    }	
 };
 
 async function fetchData (urlApi){
@@ -14,27 +16,20 @@ async function fetchData (urlApi){
     return data;
 }
 
-(async () => {
-    try {
-        const videos = await fetchData(API);
-        let view = `
-        ${videos.items.map(video => `
-            <divclass="group relative">
-                <div
-                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}"class="w-full">
-                </div>
-                <divclass="mt-4 flex justify-between">
-                    <h3 class="text-sm text-gray-700" style="color:white;">
-                        <span aria-hidden="true"class="absolute inset-0"></span>
-                        ${video.snippet.title}
-                    </h3>
-                </div>
-            </div>
-        `).slice(0, 16).join('')}
-        `;
-        content.innerHTML = view;
-  }catch (error){
-   console.log(error);
-  }
-})();
+(async()=>{try{const videos=await fetchData(API);
+    let view=`
+    ${videos.items.map(video=>`
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `).slice(0,4).join('')}
+    `;content.innerHTML=view;}catch(error){console.log(error);}})();
